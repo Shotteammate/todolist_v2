764f469778ad65bc312fd06c0799bca075e501bc
Tik San Pun <shotteammate@gmail.com>
2019-08-19 16:24:49 +0800

    Updates

---
+++ precache-manifest.170ffe28dea1735e4d1d90bffd31b4b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f668626764cb08b81ceeba3c178485a",
+    "url": "/todolist_v2/index.html"
+  },
+  {
+    "revision": "2082762ef30a9667ebe1",
+    "url": "/todolist_v2/static/css/main.c10a8922.chunk.css"
+  },
+  {
+    "revision": "04d0a18bdf54a5f5dc82",
+    "url": "/todolist_v2/static/js/2.8f15d220.chunk.js"
+  },
+  {
+    "revision": "2082762ef30a9667ebe1",
+    "url": "/todolist_v2/static/js/main.3efb3b47.chunk.js"
+  },
+  {
+    "revision": "06e30f132d6147771156",
+    "url": "/todolist_v2/static/js/runtime~main.660c8c42.js"
+  }
+]);
